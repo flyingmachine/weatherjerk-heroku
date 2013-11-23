8524562d11f368df0531b9368374408bca2380f6
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-11-23 10:53:05 -0500

    fixing routes

--- src/weatherjerk/middleware/routes.clj
+++ src/weatherjerk/middleware/routes.clj
@@ -12,8 +12,8 @@
   ;; Serve up angular app
   (apply compojure.core/routes
          (map #(compojure.core/routes
-                (GET "/" [] (compojure.route/files "index.html" {:root %}))
-                (GET "/" [] (compojure.route/resources "index.html" {:root %})))
+                (GET "/" [] (resp/file-response "index.html" {:root %}))
+                (GET "/" [] (resp/resource-response "index.html" {:root %})))
               (config :app :html-paths)))
   
   (apply compojure.core/routes
